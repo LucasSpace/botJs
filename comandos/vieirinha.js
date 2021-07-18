6e8fa83fc84d8ffce55a1f30c8d6008ed77685f8
LucasSpace <87331281+LucasSpace@users.noreply.github.com>
2021-07-18 16:04:53 -0300

    Create vieirinha.js

---
+++ comandos/vieirinha.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+ try {
+ let name = ('Vieirinha');
+
+ let avatar = {avatar: 'eu n consegui pegar a foto dele mas coloca o link da imagem q vc quiser, eu n uso essa foto e nome no meu bot'}
+
+ let vieirinha = [
+    'Sim',
+    'Com Certeza',
+    'Eu responderia mas não quero ferir seus sentimentos',
+    'Não',
+    'Do jeito que vejo, sim',
+    'Não sei, só sei que tenho um amigo chamado goiaba',
+    'Do jeito que vejo, não',
+    'Talvez sim...',
+    'Você pode acreditar nisso',
+    'Hmmmm...🤔',
+    'Acho que sim',
+    'acho que não',
+    'Não sei, só sei que moro em MamacoLandia',
+    'Provavelmente sim',
+    'Minha resposta não importa,o que importa e seguir seu coração😘',
+    'Talvez sim',
+    'Também queria saber...'
+
+];
+let arg = vieirinha[Math.floor(Math.random() * vieirinha.length)]
+
+ message.channel.createWebhook(name, avatar).then(w => { 
+ w.send(arg).then((
+ ) => w.delete())
+
+ });
+
+ } catch (err) {
+ message.reply('**Eu não tenho permissão para criar um Webhook neste servidor**')
+ }
+
+
+}
